debf2883a647e8de5100dfdb853392559def4907
Artem Sokolov <stasbasov@yandex.ru>
2022-03-10 01:31:17 +0300

    тест

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjI4ZTJjNTg3ZTZlNGMxNDQ5MmI4NDkiLCJpYXQiOjE2NDY4NjQ2NjIsImV4cCI6MTY0NzQ2OTQ2Mn0.wVjMNLPXADa-ZRNtQZ9CA0qO0Rvha885WAkmrNK4RH8', '70e2a3a9618a0c2b53ada48c02426d0714780eeff637eaae50a7c056f0dca7da');
     next(new loginError(`
     Надо исправить. В продакшне используется тот же
     секретный ключ, что и в режиме разработки.

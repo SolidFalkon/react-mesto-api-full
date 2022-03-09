ce4a933a130ae954262f5d3f743e9de9a8727e4e
Artem Sokolov <stasbasov@yandex.ru>
2022-03-10 00:59:39 +0300

    тест

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,6 +5,28 @@ const loginError = require('../errors/login-error');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
+//Тест
+const YOUR_JWT = jwt; // вставьте сюда JWT, который вернул публичный сервер студента
+const SECRET_KEY_DEV = JWT_SECRET; // вставьте сюда секретный ключ для разработки из кода студента
+try {
+const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
+console.log('\x1b[31m%s\x1b[0m', `
+Надо исправить. В продакшне используется тот же
+секретный ключ, что и в режиме разработки.
+`);
+} catch (err) {
+if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+console.log('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются');
+} else {
+console.log(
+'\x1b[33m%s\x1b[0m',
+'Что-то не так',
+err
+);
+}
+}
+
+
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;

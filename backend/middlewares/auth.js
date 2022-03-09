1402a1196e93e04ba21b3c257a20dffa97d097d9
Artem Sokolov <stasbasov@yandex.ru>
2022-03-10 01:33:35 +0300

    тест

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,23 +16,6 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjI4ZTJjNTg3ZTZlNGMxNDQ5MmI4NDkiLCJpYXQiOjE2NDY4NjQ2NjIsImV4cCI6MTY0NzQ2OTQ2Mn0.wVjMNLPXADa-ZRNtQZ9CA0qO0Rvha885WAkmrNK4RH8', '70e2a3a9618a0c2b53ada48c02426d0714780eeff637eaae50a7c056f0dca7da');
-    next(new loginError(`
-    Надо исправить. В продакшне используется тот же
-    секретный ключ, что и в режиме разработки.
-    `));
-  } catch (err) {
-    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-      next(new loginError('Всё в порядке. Секретные ключи отличаются'));
-    } else {
-      next(new loginError(
-        'Что-то не так',
-        err,
-      ));
-    }
-  }
-
-  try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     // eslint-disable-next-line new-cap

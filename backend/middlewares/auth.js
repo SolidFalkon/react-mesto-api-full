b60d34b1404cf40544b52e5f75cb84b234ce5773
Artem Sokolov <stasbasov@yandex.ru>
2022-03-10 01:18:08 +0300

    тест

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,28 +5,6 @@ const loginError = require('../errors/login-error');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-//Тест
-const YOUR_JWT = jwt; // вставьте сюда JWT, который вернул публичный сервер студента
-const SECRET_KEY_DEV = JWT_SECRET; // вставьте сюда секретный ключ для разработки из кода студента
-try {
-const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-next(new loginError('\x1b[31m%s\x1b[0m', `
-Надо исправить. В продакшне используется тот же
-секретный ключ, что и в режиме разработки.
-`));
-} catch (err) {
-if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-  next(new loginError('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются'));
-} else {
-  next(new loginError(
-'\x1b[33m%s\x1b[0m',
-'Что-то не так',
-err
-));
-}
-}
-
-
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -36,6 +14,24 @@ module.exports = (req, res, next) => {
   }
 
   let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+    next(new loginError('\x1b[31m%s\x1b[0m', `
+    Надо исправить. В продакшне используется тот же
+    секретный ключ, что и в режиме разработки.
+    `));
+  } catch (err) {
+    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+      next(new loginError('\x1b[32m%s\x1b[0m', 'Всё в порядке. Секретные ключи отличаются'));
+    } else {
+      next(new loginError(
+        '\x1b[33m%s\x1b[0m',
+        'Что-то не так',
+        err,
+      ));
+    }
+  }
 
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
